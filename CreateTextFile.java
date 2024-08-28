200e55be10889ac42b98481f783528b0ea7b1d4b
1s1sreis <159319013+1s1sreis@users.noreply.github.com>
2024-08-28 14:33:14 -0400

    Add files via upload

---
+++ CreateTextFile.java
@@ -0,0 +1,23 @@
+public class CreateTextFile{
+
+    public static void main( String[] args ){
+
+        CreateTextFile application = new CreateTextFile();
+
+        application.openFile();
+        application.addRecords();
+        application.closeFile();
+    } // fim de main
+
+    private void openFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void addRecords() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    private void closeFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+} // fim da classe CreateTextFileTest
